bc0e40d8f2c37ba4ef840920d88f0769cab49783
harrison-symes <symeshjb@gmail.com>
2018-09-12 15:04:31 +1200

    message when all gen 1 kiwis are gone

--- client/components/AvailableKiwis.jsx
+++ client/components/AvailableKiwis.jsx
@@ -5,11 +5,16 @@ import KiwiPreview from './KiwiPreview'
 
 const AvailableKiwis = ({availableKiwis}) => {
   return <div className="section has-text-centered">
-    <h2 className="title is-2">Grab a Kiwi</h2>
+    <h2 className="title is-2">Grab a 1st Generation Kiwi</h2>
     <hr />
-    <div className="columns is-multiline is-centered">
-      {availableKiwis.map((kiwi, i) => <KiwiPreview key={i} kiwi={kiwi} />)}
-    </div>
+    {availableKiwis.length > 0
+      ? <div className="columns is-multiline is-centered">
+        {availableKiwis.map((kiwi, i) => <KiwiPreview key={i} kiwi={kiwi} />)}
+      </div>
+      : <div>
+        <h2 className="subtitle is-2">Sorry! All generation 1 Kiwis have been grabbed!</h2>
+      </div>
+    }
   </div>
 }
 
