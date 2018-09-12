fc8e79619a7d15dab72f0cdb47b8ee4a09b9dce0
harrison-symes <symeshjb@gmail.com>
2018-09-13 11:03:48 +1200

    tweak randomness

--- client/createKiwi.js
+++ client/createKiwi.js
@@ -24,6 +24,6 @@ export default function createKiwi() {
     cuteness: getRandom(10),
     speed: getRandom(10),
     colour: `#${getColour()}`,
-    pursuers: getRandom(10)
+    pursuers: getRandom(10) - 1
   }
 }
