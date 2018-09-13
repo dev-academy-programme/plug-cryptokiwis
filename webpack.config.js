db0e56fa7ef00c06984cc9a0fc719f9233ad0471
harrison-symes <symeshjb@gmail.com>
2018-09-13 15:49:31 +1200

    pretty the webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,37 +2,36 @@ module.exports = {
   entry: './client/index.js',
   output: 'bundle.js',
   module: {
-    rules: [{
-      test: /\.jsx?$/,
-      loader: 'babel-loader',
-      exclude: /node_modules/
-    },
-    {
-      test: /\.(png|woff|woff2|eot|ttf|svg)$/,
-      loader: 'url-loader?limit=100000'
-    },
-    {
-      test: /\.scss$/,
-      use: [{
-        loader: "style-loader" // creates style nodes from JS strings
-      }, {
-        loader: "css-loader" // translates CSS into CommonJS
-      }, {
-        loader: "sass-loader?sourceMap" // compiles Sass to CSS
-      }]
-    }]
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        loader: 'url-loader?limit=100000'
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: "style-loader" // creates style nodes from JS strings
+          }, {
+            loader: "css-loader" // translates CSS into CommonJS
+          }, {
+            loader: "sass-loader?sourceMap" // compiles Sass to CSS
+          }
+        ]
+      }
+    ]
   },
-    resolve: {
-      extensions: ['.js', '.jsx']
-    },
-    devServer: {
-      compress: true,
-      hot: true,
-    },
-    devtool: 'source-map'
-  }
-
-  // output: {
-  //   path: __dirname + '/public',
-  //   filename: 'bundle.js'
-  // },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  devServer: {
+    compress: true,
+    hot: true,
+  },
+  devtool: 'source-map'
+}
