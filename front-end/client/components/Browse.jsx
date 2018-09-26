cd5dc45f35690466a629e2597c69874df998f678
harrison-symes <symeshjb@gmail.com>
2018-09-27 11:05:51 +1200

    hide users kiwis from browse kiwis

--- front-end/client/components/Browse.jsx
+++ front-end/client/components/Browse.jsx
@@ -4,12 +4,12 @@ import {connect} from 'react-redux'
 import KiwiPreview from './KiwiPreview'
 import KiwiList from './KiwiList'
 
-const Browse = ({kiwis}) => {
+const Browse = ({kiwis, myKiwis}) => {
 
   return <div className="section has-text-centered">
     <h2 className="title is-2">Browse Kiwis</h2>
     <hr />
-    <KiwiList kiwis={kiwis} />
+    <KiwiList kiwis={kiwis.filter(kiwi => !myKiwis.find(other => other.id == kiwi.id))} />
   </div>
 }
 
