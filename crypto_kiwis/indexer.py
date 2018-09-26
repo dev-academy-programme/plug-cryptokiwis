e254d75a7a4d372a844146f2a115ce48ada8b5c9
josephquested <josephquested@gmail.com>
2018-09-26 14:10:45 +1200

    add BreedingRequestIndexer

--- crypto_kiwis/indexer.py
+++ crypto_kiwis/indexer.py
@@ -17,3 +17,18 @@ class KiwiIndexer(PersistToFileMixin, ModelIndexer):
             del self['all'][key]
         else:
             self['all'][key]
+
+class BreedingRequestIndexer(PersistToFileMixin, ModelIndexer):
+    fqdn = "cryptoKiwi.BreedingRequestIndexer"
+
+    def update(self, key, value):
+        if '_all' not in self:
+            self['_all'] = []
+        self['_all'].append(key)
+        print('indexer[_all]', self['_all'])
+
+    def remove(self, key, value=None):
+        if value is None:
+            del self['all'][key]
+        else:
+            self['all'][key]
